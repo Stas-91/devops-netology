8335434deb26428d5dc32f7dc02b4c7bc2becf4c
Stanislav Pomelnikov <64520927+Stas-91@users.noreply.github.com>
2025-02-24 21:27:20 +0300

    Update 15-4-5-2_run_cron_backup.sh

--- 15-4-5-2_run_cron_backup.sh
+++ 15-4-5-2_run_cron_backup.sh
@@ -2,4 +2,4 @@
 
 source /opt/shvirtd-example-python/.env
 
-sudo docker run --rm --entrypoint "" -v /opt/backup:/backup --network shvirtd-example-python_backend -e "TIMESTAMP=$(date +%Y%m%d_%H%M%S)" -e "MYSQL_USER=$MYSQL_ROOT_USER" -e "MYSQL_PASSWORD=$MYSQL_ROOT_PASSWORD" schnitzler/mysqldump sh -c "mysqldump --opt -h db -u \"\$MYSQL_USER\" -p\"\$MYSQL_PASSWORD\" \"--result-file=/backup/dump_\$TIMESTAMP.sql\" virtd" \
+sudo docker run --rm --entrypoint "" -v /opt/backup:/backup --network shvirtd-example-python_backend -e "TIMESTAMP=$(date +%Y%m%d_%H%M%S)" -e "MYSQL_USER=$MYSQL_ROOT_USER" -e "MYSQL_PASSWORD=$MYSQL_ROOT_PASSWORD" schnitzler/mysqldump sh -c "mysqldump --opt -h db -u \"\$MYSQL_USER\" -p\"\$MYSQL_PASSWORD\" \"--result-file=/backup/dump_\$TIMESTAMP.sql\" virtd"
